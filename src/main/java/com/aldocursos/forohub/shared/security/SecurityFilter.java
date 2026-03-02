8759da703c1663d3cc90de1a34806698b7303e46
Aldo Serrano <aldoantonio1723@gmail.com>
2026-03-02 09:44:19 -0600

    feat: agrega la autenticacion con JWT

---
+++ src/main/java/com/aldocursos/forohub/shared/security/SecurityFilter.java
@@ -0,0 +1,52 @@
+package com.aldocursos.forohub.shared.security;
+
+import com.aldocursos.forohub.modules.usuario.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+        System.out.println(tokenJWT);
+
+        // Si se encuentra un token JWT, valida y extrae la información del usuario
+        if (tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = usuarioRepository.findByEmail(subject);
+            // Crea un objeto de autenticación con los detalles del usuario
+            var authenrtication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            // Guarda la autenticación en el contexto de seguridad de Spring
+            SecurityContextHolder.getContext().setAuthentication(authenrtication);
+        }
+
+        // Si el token es válido, continúa con la cadena de filtros
+        filterChain.doFilter(request,response);
+    }
+
+    // Extrae el token JWT del encabezado de autorización
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
+    }
+}
--- src/main/java/com/aldocursos/forohub/shared/security/TokenService.java
+++ src/main/java/com/aldocursos/forohub/shared/security/TokenService.java
@@ -4,6 +4,7 @@ import com.aldocursos.forohub.modules.usuario.Usuario;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -33,6 +34,20 @@ public class TokenService {
     }
 
     private Instant fechaExpiracion() {
-        return LocalDateTime.now().plusHours(jwtExpiration).toInstant(ZoneOffset.UTC);
+        return LocalDateTime.now().plusDays(jwtExpiration).toInstant(ZoneOffset.ofHours(-6)); // -6 horas es la hora de ciudad de mexico
+    }
+
+    // Verifica la validez del token JWT y extrae el sujeto (login del usuario)
+    public String getSubject(String tokenJWT) {
+        try {
+            var algoritmo = Algorithm.HMAC256(jwtSecret);
+            return JWT.require(algoritmo)
+                    .withIssuer("forohub")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception){
+            throw new RuntimeException("Token invalido o expirado");
+        }
     }
 }
